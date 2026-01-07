62e6cad567dfabdd7e769ee1ac27b8ff40dcfd85
ScrapRobotics <lonnysui3@gmail.com>
2026-01-07 12:19:38 -0600

    tried to fix encoders but did not work

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTesting.java
@@ -8,32 +8,38 @@ public class EncoderTesting extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Find a motor in the hardware map named "Arm Motor"
-        DcMotor motor = hardwareMap.dcMotor.get("EncoderTester");
+        DcMotor motor = hardwareMap.dcMotor.get("BLM");
 
         // Reset the motor encoder so that it reads zero ticks
-        //motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        //motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         // Turn the motor back on, required if you use STOP_AND_RESET_ENCODER
-        //motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-
+        motor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         waitForStart();
 
         while (opModeIsActive()) {
-            //double CPR = [Your Counts Per Revolution Here];
+            double CPR = motor.getMotorType().getTicksPerRev();
 
             // Get the current position of the motor
             int position = motor.getCurrentPosition();
-            //double revolutions = position/CPR;
+            double revolutions = position/CPR;
 
-            //double angle = revolutions * 360;
-            //double angleNormalized = angle % 360;
-
+            double angle = revolutions * 360;
+            double angleNormalized = angle % 360;
+            if(gamepad1.a) {
+                motor.setPower(1);
+            }
+            else {
+                motor.setPower(0);
+            }
             // Show the position of the motor on telemetry
             telemetry.addData("Encoder Position", position);
             telemetry.addData("CPR: ", motor.getMotorType().getTicksPerRev());
-            //telemetry.addData("Encoder Revolutions", revolutions);
-            //telemetry.addData("Encoder Angle (Degrees)", angle);
-            //telemetry.addData("Encoder Angle - Normalized (Degrees)", angleNormalized);
+            telemetry.addData("Encoder Revolutions", revolutions);
+            telemetry.addData("Encoder Angle (Degrees)", angle);
+            telemetry.addData("Encoder Angle - Normalized (Degrees)", angleNormalized);
+            telemetry.addData("CPR: ", motor.getMotorType().getTicksPerRev());
             telemetry.update();
             //ian code kl;'lk;lk;nlmknmlkj;l
         }
