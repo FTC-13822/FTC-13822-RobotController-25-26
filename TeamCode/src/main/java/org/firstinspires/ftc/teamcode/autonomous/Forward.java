e09ea52910aef1ad40890dac23a2760d78c3a492
E-C18 <ethanc2443@gmail.com>
2026-01-22 15:55:45 -0600

    move forward auton - acceleration part 2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Forward.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+public class Forward extends LinearOpMode{
+    public DcMotor BLM;
+    public DcMotor FRM;
+    public DcMotor FLM;
+    public DcMotor BRM;
+    ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode(){
+
+    }
+    public void moveF(int distance){
+        BRM = hardwareMap.get(DcMotor.class, "BRM");
+        BLM = hardwareMap.get(DcMotor.class, "BLM");
+        FRM = hardwareMap.get(DcMotor.class, "FRM");
+        FLM = hardwareMap.get(DcMotor.class, "FLM");
+        BRM.setTargetPosition(distance);
+        FRM.setTargetPosition(distance);
+        BLM.setTargetPosition(-distance);
+        FLM.setTargetPosition(-distance);
+        FRM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FRM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BRM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BRM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BLM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BLM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FLM.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FLM.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        double CPR  = 384.5;
+        double diameter = 96;
+        double circumference = Math.PI*diameter;
+        double position = FRM.getCurrentPosition();
+        double revolutions = position/CPR;
+        double distanceMoved = revolutions *circumference;
+        double power = FRM.getPower();
+        //acceleration
+        while (distanceMoved < distance && power < 0.75) {
+            power +=0.05;
+            FRM.setPower(power);
+            FLM.setPower(-power);
+            BRM.setPower(power);
+            BLM.setPower(-power);
+        }
+    }
+
+
+}
