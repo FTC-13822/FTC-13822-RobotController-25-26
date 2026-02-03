9fb66139086035b3f7fdb117ec66d4c9d23399d1
ScrapRobotics <lonnysui3@gmail.com>
2026-02-03 16:35:54 -0600

    intake for auton

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Intake.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+
+public class Intake extends OpMode {
+
+    public CRServo Servo1;
+    public CRServo Servo2;
+    public CRServo Servo3;
+    public ElapsedTime runTime;
+
+    public Intake(HardwareMap hardwareMap) {
+        Servo1 = hardwareMap.get(CRServo.class,"Servo1");
+        Servo2 = hardwareMap.get(CRServo.class,"Servo2");
+        Servo3 = hardwareMap.get(CRServo.class,"Servo3");
+        ElapsedTime runTime = new ElapsedTime();
+    }
+    @Override
+    public void init(){
+    }
+    @Override
+    public void loop(){
+
+    }
+    public void takein(int time) {
+        runTime.reset();
+        while(runTime.seconds()<time) {
+            Servo1.setPower(1);
+            Servo2.setPower(1);
+            Servo3.setPower(1);
+        }
+        Servo1.setPower(0);
+        Servo2.setPower(0);
+        Servo3.setPower(0);
+    }
+}
