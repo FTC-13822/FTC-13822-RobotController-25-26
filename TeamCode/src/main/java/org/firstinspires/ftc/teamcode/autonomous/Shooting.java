a8b5df079536d1ef751ea5c786327ab0ff042239
Ethan C <Ethanc2443@gmail.com>
2026-02-07 08:53:51 -0600

    changed shooting auton. Servos (intake) will run continuously to feed the shooter.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Shooting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Shooting.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -10,12 +11,18 @@ public class Shooting extends OpMode {
     public DcMotor ShooterPrecision;
     public DcMotor ShooterPower;
     public ElapsedTime runTime;
+    Intake intake;
+    public CRServo Servo1;
+    public CRServo Servo2;
 
     public Shooting(HardwareMap hardwareMap) {
 
         ShooterPrecision = hardwareMap.get(DcMotor.class, ("Shooter1"));
         ShooterPower = hardwareMap.get(DcMotor.class, ("Shooter2"));
+        Servo1 = hardwareMap.get(CRServo.class,"Servo1");
+        Servo2 = hardwareMap.get(CRServo.class,"Servo2");
         runTime = new ElapsedTime();
+        intake = new Intake(hardwareMap);
     }
     @Override
     public void init(){
@@ -27,10 +34,15 @@ public class Shooting extends OpMode {
     public void outtake(int time){
         runTime.reset();
         while (runTime.seconds()<time){
+            Servo1.setPower(1);
+            Servo2.setPower(1);
             ShooterPower.setPower(1);
             ShooterPrecision.setPower(1);
+
         }
         ShooterPower.setPower(0);
         ShooterPrecision.setPower(0);
+        Servo1.setPower(0);
+        Servo2.setPower(0);
     }
 }
