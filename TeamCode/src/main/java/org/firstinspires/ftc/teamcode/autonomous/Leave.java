67bc9030b2fb4a4e37620d990930bbd99d3e4197
Ethan C <Ethanc2443@gmail.com>
2026-02-07 14:18:17 -0600

    Made separate class for Leave - auton.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/Leave.java
@@ -0,0 +1,50 @@
+package org.firstinspires.ftc.teamcode.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+@TeleOp
+public class Leave extends OpMode {
+    boolean end = false;
+    Back back;
+    Forward forward;
+    left moveLeft;
+    right moveRight;
+    Shooting shot;
+    Intake intake;
+    DcMotor BRM;
+    DcMotor FRM;
+    DcMotor BLM;
+    DcMotor FLM;
+    //initialize both motors
+    DcMotor ShooterPrecision;
+    DcMotor ShooterPower;
+    private CRServo Servo1;
+    private CRServo Servo3;
+
+    @Override
+    public void init(){
+        forward = new Forward(hardwareMap);
+        moveLeft = new left(hardwareMap);
+        moveRight = new  right(hardwareMap);
+        shot = new Shooting(hardwareMap);
+        intake = new Intake(hardwareMap);
+        back = new Back(hardwareMap);
+    }
+
+    @Override
+    public void loop(){
+        if(end == false) {
+            forward.moveF(1100);
+            //back.moveB(1000);
+            //moveLeft.turnLeft(360);
+            //moveRight.turnRight(360);
+            //intake.takein(10);
+            //shot.outtake(3);
+        }
+        end = true;
+    }
+}
